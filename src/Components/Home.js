a0ab6097fec5cfa4963244d8e897241902cb2984
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-08 11:32:59 -0600

    moved port to 3001 / Home.js render

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,36 +1,43 @@
- import React, { useState } from "react";
- import { listDecks } from "../utils/api";
+import React, { useState,useEffect } from "react";
+import { listDecks } from "../utils/api";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const [decks, setDecks] = useState([]);
+     const [decks, setDecks] = useState([]);
 
-// function loadDecks() {
-//   listDecks().then(setDecks)
-// }
+  const loadDecks = () => {
+    listDecks().then(setDecks)
+  }
 
-const loadDecks = () => {
-  listDecks().then(setDecks)
-}
+  useEffect(() => {
+    loadDecks()
 
-  // return (
-  //   <>
-  //     <button>Create Deck</button>
-  //     <div>
-  //       <fieldset>
-  //         <h3>Deck Name</h3>
-  //         <p> # cards</p>
-  //         <p>This would be the deck description</p>
-  //         <div>
-  //           <button>View</button>
-  //           <button>Study</button>
-  //           <button>Trash</button>
-  //         </div>
-  //       </fieldset>
-  //     </div>
-  //   </>
-  // );
+  }, []);
 
   return (
-    <button>Hello?</button>
-  )
+    <>
+      <Link href="#" class="btn btn-secondary mb-3">
+        <span class="oi oi-plus"></span> Create Deck</Link>
+      {decks.map(deck => (
+           <div class="card mb-3">
+           <div class="card-body">
+             <div className="row">
+               <div className="col">
+                 <h5 class="card-title">{deck.name}</h5>
+               </div>
+               <div className="col">
+                 <p className="float-right">{deck.cards.length} cards</p>
+               </div>
+             </div>
+             <p class="card-text">
+               {deck.description}
+             </p>
+             <Link href="#" class="btn btn-secondary mr-2"><span class="oi oi-eye"></span> View</Link>
+             <Link href="#" class="btn btn-primary"> <span class="oi oi-book"></span> Study</Link>
+             <Link href="#" class="btn btn-danger float-right"><span class="oi oi-trash"></span> </Link>
+           </div>
+         </div>
+      ))}
+    </>
+  );
 }
