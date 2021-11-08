6e0c3e73ddfc70b97e5d52244ef4632a12ec45e2
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-08 10:12:33 -0600

    initial setup

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { listDecks } from "../utils/api";
+ import React, { useState } from "react";
+ import { listDecks } from "../utils/api";
 
 export default function Home() {
   const [decks, setDecks] = useState([]);
 
+// function loadDecks() {
+//   listDecks().then(setDecks)
+// }
+
+const loadDecks = () => {
+  listDecks().then(setDecks)
+}
+
+  // return (
+  //   <>
+  //     <button>Create Deck</button>
+  //     <div>
+  //       <fieldset>
+  //         <h3>Deck Name</h3>
+  //         <p> # cards</p>
+  //         <p>This would be the deck description</p>
+  //         <div>
+  //           <button>View</button>
+  //           <button>Study</button>
+  //           <button>Trash</button>
+  //         </div>
+  //       </fieldset>
+  //     </div>
+  //   </>
+  // );
+
   return (
-    <>
-      <button>Create Deck</button>
-      <div>
-        <fieldset>
-          <h3>Deck Name</h3>
-          <p> # cards</p>
-          <p>This would be the deck description</p>
-          <div>
-            <button>View</button>
-            <button>Study</button>
-            <button>Trash</button>
-          </div>
-        </fieldset>
-      </div>
-    </>
-  );
+    <button>Hello?</button>
+  )
 }
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
 import { Route, Switch } from "react-router-dom";
-import Home from "../Home/Home";
+import Home from "../Components/Home";
+import ViewDeck from "../Components/Decks/ViewDeck"
+import EditDeck from "../Components/Decks/EditDeck"
+import CreateDeck from "../Components/Decks/CreateDeck"
+import StudyDeck from "../Components/Decks/StudyDeck"
+import AddCard from "../Components/Cards/AddCard"
+import EditCard from "../Components/Cards/EditCard"
+
+
 function Layout() {
   return (
     <>
@@ -12,6 +20,27 @@ function Layout() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/decks">
+            <Home />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <StudyDeck />
+          </Route>
+          <Route path="/decks/new">
+            <CreateDeck />
+          </Route>
+          <Route exact path="/decks/:deckId">
+            <ViewDeck />
+          </Route>
+          <Route path="/decks/:deckId/edit">
+            <EditDeck />
+          </Route>
+          <Route path="/decks/:deckId/cards/new">
+            <AddCard />
+          </Route>
+          <Route path="/decks/:deckId/cards/:cardId/edit">
+            <EditCard />
           </Route>
           <Route>
             <NotFound />
