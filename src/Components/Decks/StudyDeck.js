82b3a857ef2d1a8d4dd79f153f63ba396b48c580
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-08 22:12:50 -0600

    end of deck message to restarT

--- src/Components/Decks/StudyDeck.js
+++ src/Components/Decks/StudyDeck.js
@@ -1,30 +1,69 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, useHistory } from "react-router-dom"
 import React, { useState, useEffect } from "react";
 import { readDeck } from "../../utils/api"
 
 export default function StudyDeck() {
+    const history = useHistory();
     const {deckId} = useParams();
     const [deck, setDeck] = useState({cards: []});
-    // const [side, setSide] = useState("front")
-    // const [currentCard, setCurrentCard] = useState(0)
+    const [frontSide, setFrontSide] = useState(true)
     const [cards, setCards] = useState([]);
+    const [index, setIndex] = useState(0)
 
 
     const loadDeck = () => {
       readDeck(deckId).then(setDeck)
       setCards(deck.cards)
     }
+
+    const flipHandler = () => {
+        setFrontSide(!frontSide)
+    }
+
+    const nextHandler = () => {
+        if (index === cards.length - 1) {
+            const restart = window.confirm(
+                "Restart cards?/n/nClick 'cancel' to return to the home page."
+            )
+            if (restart) {
+                // why did I have to set the index to -1?
+                setIndex(-1)
+                setFrontSide(true)
+            } else {
+                history.push("/");
+            }
+        }
+
+        setIndex((index) => index + 1)
+        setFrontSide(true)
+    }
   
     useEffect(() => {
         loadDeck()
-    
       // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [deck.id]);
+        
 
-    
+    if (cards.length <= 2) {
+        return (
+            <>
+            <div className="breacrumbs">
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item"><Link to="/"><span className="oi oi-home"></span> Home</Link></li>
+                        <li className="breadcrumb-item"><Link to={`/decks/${deck.id}`}>{deck.name}</Link></li>
+                        <li className="breadcrumb-item active" aria-current="page">Study</li>
+                    </ol>
+                </nav>
+            </div>
+            <h1>{deck.name}: Study</h1>
+            <h2>Not enough cards.</h2>
+            <p>You need at least 3 cards to study. There are {cards.length} cards in this deck.</p>
+            <Link to={`/decks/${deck.id}/cards/new`} className="btn btn-primary"><span className="oi oi-plus"></span> Add Cards</Link>
+            </>
+        )
+    }
 
-
-   // console.log(cards)
 
     return (
         <>
@@ -38,8 +77,8 @@ export default function StudyDeck() {
             </nav>
         </div>
         <h1>Study: {deck.name}</h1>
-             {cards.map((card, index) => (
-                        <div className="card mb-3">
+
+                    <div className="card mb-3">
                         <div className="card-body">
                             <div className="row">
                                 <div className="col">
@@ -47,12 +86,12 @@ export default function StudyDeck() {
                                 </div>
                             </div>
                             <p className="card-text">
-                                {card.front}
+                            {frontSide ? cards[index].front : cards[index].back}
                             </p>
-                            <button type="button" className="btn btn-secondary" >Flip</button>
+                            <button type="button" className="btn btn-secondary mr-2" onClick={flipHandler} >Flip</button>
+                            {frontSide === false && <button type="button" className="btn btn-primary" onClick={nextHandler} >Next</button>}
                         </div>
-                        </div>
-             ))}
+                    </div>
             </>
     )
 }
